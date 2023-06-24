98ac4ea0b0ffa24e8e072b04d4cbc6a17ce69fc2
sharathjr <sharathjrr@gmail.com>
2023-06-23 22:31:11 -0400

    errors removed

--- src/main/java/com/training/examples/Lorry.java
+++ src/main/java/com/training/examples/Lorry.java
@@ -5,27 +5,7 @@ import java.util.Optional;
 public class Lorry {
 
 	public static void main(String args[]) {
-		    Integer year = 2016;
-		    Optional<Integer> yearOptional = Optional.of(year);
-		    boolean is2016 = yearOptional.filter(y -> y == 2016).isPresent();
-		    boolean is2017 = yearOptional.filter(y -> y == 2017).isPresent();
-		    
-		    System.out.println("is2016 "+is2016);
-		    System.out.println("is2017 "+is2017);
-		    
-		    String nullName = "s";
-		    String name = Optional.ofNullable(nullName).orElseThrow(
-		      IllegalArgumentException::new);
-		    
-		    User u = u.orElseThrow(UserDetailsNotFOund::new)
-		    
-		    System.out.println("name "+name);
-		    
-		    if() {
-		    	
-		    }else {
-		    	
-		    }
+		   
 	}
 
 }
