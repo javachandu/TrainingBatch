dd7d943c3c5768c4055513bd0b7fc9da3e1d8249
sharath123t <sharathchandrat96@gmail.com>
2023-06-26 11:55:07 -0500

    Create EvenNo.java
    evenno

---
+++ src/main/java/com/training/examples1/EvenNo.java
@@ -0,0 +1,20 @@
+package com.training.examples1;
+
+public class EvenNo 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int Max = 50;
+		System.out.println("Even No from 1 to" + Max + ":");
+		for(int i =2; i<=Max ; i++) 
+		{
+			if(i % 2 == 0)
+			{
+				System.out.println("i");
+			}
+		}
+	}
+
+}
